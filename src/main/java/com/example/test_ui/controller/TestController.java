f77c03d7e7d022c59dc472ad01fdad619f18975a
shoon0615 <shoon0615@gmail.com>
2023-05-24 01:03:20 +0900

    git 테스트 213423

--- src/main/java/com/example/test_ui/controller/TestController.java
+++ src/main/java/com/example/test_ui/controller/TestController.java
@@ -34,8 +34,8 @@ public class TestController {
         return "/test_ui/admin/main";
     }
 
-    @GetMapping("/admin4")
-    public String admin4(Model model) {
+    @GetMapping("/admin6")
+    public String admin6(Model model) {
         return "/test_ui/admin/main";
     }
 
